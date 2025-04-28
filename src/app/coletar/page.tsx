9e5b963fc176c5137c058ae99b81a31f64d961f3
leandro641A <leandroimobil@gmail.com>
2025-04-28 00:20:44 -0300

    Create page.tsx

---
+++ src/app/coletar/page.tsx
@@ -0,0 +1,19 @@
+{
+  "name": "coletor-promocoes",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint"
+  },
+  "dependencies": {
+    "next": "14.0.4",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "axios": "^1.9.0",
+    "cheerio": "^1.0.0",
+    "puppeteer": "^24.7.2"
+  }
+}
